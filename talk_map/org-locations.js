16010611b4b9593d4a8fd1d60f11a11b22a27207
twolfe <thomsa.wolfe@univie.ac.at>
2019-01-29 10:29:20 +0100

    work6

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,11 +1,11 @@
 var addressPoints = [
   [
-    "Fort Worth, Texas | Botany | <a href='http://twolfe.github.io/talks/talks/2018-07-25-botany'>Botany Conference</a>",
+    "Fort Worth, Texas | Botany | <a href='http://twolfe.github.io/talks/2018-07-25-botany'>Botany Conference</a>",
     32.758255,
     -97.326859
   ],
   [
-    "Cambridge, UK | PopGroup | <a href='http://twolfe.github.io/talks/talks/2018-01-04-popgroup'>PopGroup Conference</a>",
+    "Cambridge, UK | PopGroup | <a href='http://twolfe.github.io/talks/2018-01-04-popgroup'>PopGroup Conference</a>",
     52.205337,
     0.121817
   ]
