8589ab3846f0d6b8c1a78d6d1ee10b54067e7fe4
twolfe <thomsa.wolfe@univie.ac.at>
2019-01-30 14:24:11 +0100

    work

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -8,5 +8,10 @@ var addressPoints = [
     "Cambridge, UK | PopGroup | <a href='http://twolfe.github.io/talks/2018-01-04-popgroup'>PopGroup Conference</a>",
     52.205337,
     0.121817
+  ],
+  [
+    "Rovinj, Croatia | International Conference on Polyploidy, Hybridization and Biodiversity | <a href='http://twolfe.github.io//talks/2016-05-11-icp'>Polyploidy Conference</a>",
+    45.081166,
+    13.638707
   ]
 ];
