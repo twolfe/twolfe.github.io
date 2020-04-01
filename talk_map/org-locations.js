457651bcc9bb170e75c1efdb1f582f6eca27c25a
twolfe <thomsa.wolfe@univie.ac.at>
2020-04-01 14:47:28 +0200

    apr2020

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -18,20 +18,20 @@ var addressPoints = [
     "Vienna, Austria | 48th Annual Meeting of the Ecological Society of Germany, Austria and Switzerland | <a href='http://twolfe.github.io//talks/2018-09-10-ecologicalSoc'>Ecology Conference</a>",
     48.231554,
     16.357586
-  ]
+  ],
   [
     "Vienna, Austria | Plant epigenetics: from mechanisms to ecological relevance | <a href='https://www.newphytologist.org/blog/plant-epigenetics-mechanisms-to-ecological-relevance/'>Epigenetics Conference</a>",
     48.193850,
     16.384914
-  ]
+  ],
   [
     "Sitges, Spain | Plant Genome Evolution 2017 | <a href='https://www.elsevier.com/events/conferences/plant-genome-evolution'>Plant Genomics Conference</a>",
     41.236458,
     1.823892
-  ]
+  ],
   [
     "Montpellier, France | II Joint Congress on Evolutionary Biology (ESEB) | <a href='https://eseb.org/wp-content/uploads/2018/08/2018_Program.pdf'>Evolutionary Biology Conference</a>",
-    41.236458,
-    1.823892
+    43.613047,
+    3.882193
   ]
 ];
